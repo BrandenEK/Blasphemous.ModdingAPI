40dd058579048fe3fd93a6533b13f83b095059c5
Branden Wood <brandenek03912@hotmail.com>
2023-02-15 15:19:55 -0500

    Initial upload files

---
+++ ModdingAPI/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ModdingAPI
+{
+    [BepInPlugin("com.damocles.blasphemous.modding-api", "Modding API", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony harmony = new Harmony("com.damocles.blasphemous.modding-api");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Blasphemoous Modding API
+
+To create a new project, open the command line and run:
+```dotnet new bepinex5plugin -n ModdingAPI -T net35 -U 2017.4.40```
