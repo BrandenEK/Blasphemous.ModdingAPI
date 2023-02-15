2a557614a08722b49f3fec10ff47125d19def6ab
Branden Wood <brandenek03912@hotmail.com>
2023-02-15 15:55:45 -0500

    Add moddingAPI class and call all functions

--- ModdingAPI/Main.cs
+++ ModdingAPI/Main.cs
@@ -7,10 +7,59 @@ namespace ModdingAPI
     [BepInProcess("Blasphemous.exe")]
     public class Main : BaseUnityPlugin
     {
+        public static ModdingAPI moddingAPI;
+        private static Main instance;
+
         private void Awake()
+        {
+            instance = this;
+            Patch();
+
+            moddingAPI = new ModdingAPI();
+            moddingAPI.Awake();
+        }
+
+        private void Start() { moddingAPI.Start(); }
+
+        private void Update() { moddingAPI.Update(); }
+
+        private void LateUpdate() { moddingAPI.LateUpdate(); }
+
+        // Patch all modding api methods
+        private void Patch()
         {
             Harmony harmony = new Harmony("com.damocles.blasphemous.modding-api");
             harmony.PatchAll();
+        }
+
+        // Log messages to unity console
+        public static void LogMessage(string message)
+        {
+            instance.logMessage(message);
+        }
+        private void logMessage(string message)
+        {
+            Logger.LogMessage(message);
+        }
+
+        // Log warnings to unity console
+        public static void LogWarning(string message)
+        {
+            instance.logWarning(message);
+        }
+        private void logWarning(string message)
+        {
+            Logger.LogWarning(message);
+        }
+
+        // Log errors to unity console
+        public static void LogError(string message)
+        {
+            instance.logError(message);
+        }
+        private void logError(string message)
+        {
+            Logger.LogError(message);
         }
     }
 }
---
+++ ModdingAPI/ModdingAPI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModdingAPI
+{
+    public class ModdingAPI
+    {
+        public void Awake()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void LateUpdate()
+        {
+
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
