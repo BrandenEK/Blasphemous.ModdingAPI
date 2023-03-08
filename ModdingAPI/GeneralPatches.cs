f2d22455ed9a1a44b694f9b44a5a6ec95770e4e0
Branden Wood <brandenek03912@hotmail.com>
2023-03-08 13:03:24 -0500

    Move files to subfolders

---
+++ ModdingAPI/GeneralPatches.cs
@@ -0,0 +1,59 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using Framework.Managers;
+using Gameplay.UI.Others.MenuLogic;
+using System.Text;
+
+namespace ModdingAPI
+{
+    // Initialize mods
+    [HarmonyPatch(typeof(AchievementsManager), "AllInitialized")]
+    internal class AchievementsManager_InitializePatch
+    {
+        public static void Postfix()
+        {
+            Main.moddingAPI.Initialize();
+        }
+    }
+    // Dispose mods
+    [HarmonyPatch(typeof(AchievementsManager), "Dispose")]
+    internal class AchievementsManager_DisposePatch
+    {
+        public static void Postfix()
+        {
+            Main.moddingAPI.Dispose();
+        }
+    }
+
+    // Add mod names to main menu
+    [HarmonyPatch(typeof(VersionNumber), "Start")]
+    internal class VersionNumber_Patch
+    {
+        public static void Postfix(VersionNumber __instance)
+        {
+            Text version = __instance.GetComponent<Text>();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0} v{1}\n", Main.MOD_NAME, Main.MOD_VERSION);
+
+            foreach (Mod mod in Main.moddingAPI.getMods())
+            {
+                sb.AppendFormat("{0} v{1}\n", mod.ModName, mod.ModVersion);
+            }
+
+            version.alignment = TextAnchor.UpperRight;
+            version.text = sb.ToString();
+        }
+    }
+
+    // Call new game when game is first started
+    [HarmonyPatch(typeof(NewMainMenu), "InternalPlay")]
+    internal class NewMainMenu_Patch
+    {
+        public static void Postfix(bool ___isContinue)
+        {
+            if (!___isContinue)
+                Main.moddingAPI.NewGame();
+        }
+    }
+}
--- ModdingAPI/Skins/SkinPatches.cs
+++ ModdingAPI/Skins/SkinPatches.cs
@@ -1,125 +1,16 @@
 ﻿using HarmonyLib;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using Framework.Managers;
-using Gameplay.UI.Widgets;
-using Gameplay.UI.Console;
 using Gameplay.UI.Others;
 using Gameplay.UI.Others.MenuLogic;
 using Gameplay.GameControllers.Effects.Player.Recolor;
 using Gameplay.UI.Others.Buttons;
-using System.Text;
 using System.Collections.Generic;
 using I2.Loc;
 
 namespace ModdingAPI
 {
-    // Initialize mods
-    [HarmonyPatch(typeof(AchievementsManager), "AllInitialized")]
-    internal class AchievementsManager_InitializePatch
-    {
-        public static void Postfix()
-        {
-            Main.moddingAPI.Initialize();
-        }
-    }
-    // Dispose mods
-    [HarmonyPatch(typeof(AchievementsManager), "Dispose")]
-    internal class AchievementsManager_DisposePatch
-    {
-        public static void Postfix()
-        {
-            Main.moddingAPI.Dispose();
-        }
-    }
-
-    // Add mod names to main menu
-    [HarmonyPatch(typeof(VersionNumber), "Start")]
-    internal class VersionNumber_Patch
-    {
-        public static void Postfix(VersionNumber __instance)
-        {
-            Text version = __instance.GetComponent<Text>();
-            StringBuilder sb = new StringBuilder();
-            sb.AppendFormat("{0} v{1}\n", Main.MOD_NAME, Main.MOD_VERSION);
-
-            foreach (Mod mod in Main.moddingAPI.getMods())
-            {
-                sb.AppendFormat("{0} v{1}\n", mod.ModName, mod.ModVersion);
-            }
-
-            version.alignment = TextAnchor.UpperRight;
-            version.text = sb.ToString();
-        }
-    }
-
-    // Allow access to console
-    [HarmonyPatch(typeof(ConsoleWidget), "Update")]
-    internal class ConsoleWidgetEnable_Patch
-    {
-        public static void Postfix(ConsoleWidget __instance, bool ___isEnabled)
-        {
-            if (Input.GetKeyDown(KeyCode.Backslash))
-            {
-                __instance.SetEnabled(!___isEnabled);
-            }
-        }
-    }
-
-    // Allow console commands on the main menu
-    [HarmonyPatch(typeof(KeepFocus), "Update")]
-    internal class KeepFocusMain_Patch
-    {
-        public static bool Prefix()
-        {
-            return ConsoleWidget.Instance == null || !ConsoleWidget.Instance.IsEnabled();
-        }
-    }
-    [HarmonyPatch(typeof(ConsoleWidget), "SetEnabled")]
-    internal class ConsoleWidgetDisable_Patch
-    {
-        public static void Postfix(bool enabled)
-        {
-            if (!enabled && Core.LevelManager.currentLevel.LevelName == "MainMenu")
-            {
-                Button[] buttons = Object.FindObjectsOfType<Button>();
-                foreach (Button b in buttons)
-                {
-                    if (b.name == "Continue")
-                    {
-                        EventSystem.current.SetSelectedGameObject(b.gameObject);
-                        return;
-                    }
-                }
-            }
-        }
-    }
-
-    // Add custom mod commands to the console
-    [HarmonyPatch(typeof(ConsoleWidget), "InitializeCommands")]
-    internal class ConsoleWidgetInitialize_Patch
-    {
-        public static void Postfix(List<ConsoleCommand> ___commands)
-        {
-            foreach (ModCommand command in Main.moddingAPI.getModCommnds())
-            {
-                ___commands.Add(new ModCommandSystem(command));
-            }
-        }
-    }
-
-    // Call new game when game is first started
-    [HarmonyPatch(typeof(NewMainMenu), "InternalPlay")]
-    internal class NewMainMenu_Patch
-    {
-        public static void Postfix(bool ___isContinue)
-        {
-            if (!___isContinue)
-                Main.moddingAPI.NewGame();
-        }
-    }
-
     // Load custom skins from folder
     [HarmonyPatch(typeof(ColorPaletteManager), "Initialize")]
     internal class ColorPaletteManagerInit_Patch
