c3dfe0ba4b63112bd2421a74f75e476cbb894802
Branden Wood <brandenek03912@hotmail.com>
2023-03-10 16:03:03 -0500

    Add custom item classes

---
+++ ModdingAPI/Items/ItemPatches.cs
@@ -0,0 +1,9 @@
+﻿using HarmonyLib;
+using Framework.Managers;
+
+namespace ModdingAPI
+{
+    // Register items on init
+    // Add slots to inventory
+    // Maybe methods for adding them ?
+}
---
+++ ModdingAPI/Items/ModRosaryBead.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace ModdingAPI
+{
+    public class ModRosaryBead
+    {
+        protected internal string Id { get; }
+
+        protected internal string Name { get; }
+
+        protected internal string Description { get; }
+
+        protected internal string Lore { get; }
+
+        protected internal bool CarryOnStart { get; }
+
+        protected internal bool PreserveInNGPlus { get; }
+
+        protected internal bool AddToPercentageCompletion { get; }
+
+        internal Sprite Picture { get; private set; }
+
+        internal List<ModItemEffect> Effects { get; private set; }
+
+        public ModRosaryBead SetImage(Sprite image)
+        {
+            Picture = image;
+            return this;
+        }
+
+
+        public ModRosaryBead AddEffect<T>() where T : ModItemEffect, new()
+        {
+            Effects.Add(new T());
+            return this;
+        }
+    }
+}
