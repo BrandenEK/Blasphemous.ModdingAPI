ff3fe795946d821c801f13fd4af2e7eb947b2214
Branden Wood <brandenek03912@hotmail.com>
2023-03-28 17:02:57 -0400

    Allow input to check axes

--- ModdingAPI/InputHandler.cs
+++ ModdingAPI/InputHandler.cs
@@ -12,7 +12,7 @@ namespace ModdingAPI
         /// </summary>
         public enum ButtonCode
         {
-            #pragma warning disable CS1591
+#pragma warning disable CS1591
             // Face buttons
             Attack = 5,
             Jump = 6,
@@ -33,7 +33,6 @@ namespace ModdingAPI
             DialogNext = 35,
             DialogSkip = 39,
             // UI navigation
-            UIVertical = 49,
             UISubmit = 50,
             UICancel = 51,
             UIContextual = 52,
@@ -43,7 +42,22 @@ namespace ModdingAPI
             Pause = 10,
             Inventory = 22,
             GrabCancel = 65,
-            #pragma warning restore CS1591
+#pragma warning restore CS1591
+        }
+
+        /// <summary>
+        /// The axis code for each input action
+        /// </summary>
+        public enum AxisCode
+        {
+#pragma warning disable CS1591
+            MoveHorizontal = 0,
+            MoveVertical = 4,
+            MoveRHorizontal = 21,
+            MoveRVertical = 20,
+            UIHorizontal = 48,
+            UIVertical = 49,
+#pragma warning restore CS1591
         }
 
         private Player rewired;
@@ -60,58 +74,64 @@ namespace ModdingAPI
         /// <summary>
         /// Checks whether a button is currently being held down
         /// </summary>
-        /// <param name="btn">The button code to check for</param>
+        /// <param name="button">The button code to check for</param>
         /// <returns>The button status</returns>
-        public bool GetButton(int btn)
+        public bool GetButton(ButtonCode button)
         {
-            return Rewired != null && Rewired.GetButton(btn);
-        }
-        /// <summary>
-        /// Checks whether a button is currently being held down
-        /// </summary>
-        /// <param name="btn">The button code to check for</param>
-        /// <returns>The button status</returns>
-        public bool GetButton(ButtonCode btn)
-        {
-            return GetButton((int)btn);
+            return Rewired != null && Rewired.GetButton((int)button);
         }
 
         /// <summary>
         /// Checks whether a button was pressed during this frame
         /// </summary>
-        /// <param name="btn">The button code to check for</param>
+        /// <param name="button">The button code to check for</param>
         /// <returns>The button status</returns>
-        public bool GetButtonDown(int btn)
+        public bool GetButtonDown(ButtonCode button)
         {
-            return Rewired != null && Rewired.GetButtonDown(btn);
-        }
-        /// <summary>
-        /// Checks whether a button was pressed during this frame
-        /// </summary>
-        /// <param name="btn">The button code to check for</param>
-        /// <returns>The button status</returns>
-        public bool GetButtonDown(ButtonCode btn)
-        {
-            return GetButtonDown((int)btn);
+            return Rewired != null && Rewired.GetButtonDown((int)button);
         }
 
         /// <summary>
         /// Checks whether a button was released during this frame
         /// </summary>
-        /// <param name="btn">The button code to check for</param>
+        /// <param name="button">The button code to check for</param>
         /// <returns>The button status</returns>
-        public bool GetButtonUp(int btn)
+        public bool GetButtonUp(ButtonCode button)
         {
-            return Rewired != null && Rewired.GetButtonUp(btn);
+            return Rewired != null && Rewired.GetButtonUp((int)button);
         }
+
         /// <summary>
-        /// Checks whether a button was released during this frame
+        /// Checks the direction that an axis is held at from -1 to 1
         /// </summary>
-        /// <param name="btn">The button code to check for</param>
-        /// <returns>The button status</returns>
-        public bool GetButtonUp(ButtonCode btn)
+        /// <param name="axis">The axis code the check for</param>
+        /// <param name="useRawInput">Whether to use the raw axis data</param>
+        /// <returns>The axis status</returns>
+        public float GetAxis(AxisCode axis, bool useRawInput)
         {
-            return GetButtonUp((int)btn);
+            return Rewired != null && useRawInput ? Rewired.GetAxisRaw((int)axis) : Rewired.GetAxis((int)axis);
+        }
+
+        /// <summary>
+        /// Checks the direction that an axis was held at on the last frame from -1 to 1
+        /// </summary>
+        /// <param name="axis">The axis code the check for</param>
+        /// <param name="useRawInput">Whether to use the raw axis data</param>
+        /// <returns>The axis status</returns>
+        public float GetAxisPrevious(AxisCode axis, bool useRawInput)
+        {
+            return Rewired != null && useRawInput ? Rewired.GetAxisRawPrev((int)axis) : Rewired.GetAxisPrev((int)axis);
+        }
+
+        /// <summary>
+        /// Checks the time that an axis has been held
+        /// </summary>
+        /// <param name="axis">The axis code the check for</param>
+        /// <param name="useRawInput">Whether to use the raw axis data</param>
+        /// <returns>The axis status</returns>
+        public float GetAxisTimeActive(AxisCode axis, bool useRawInput)
+        {
+            return Rewired != null && useRawInput ? Rewired.GetAxisRawTimeActive((int)axis) : Rewired.GetAxisTimeActive((int)axis);
         }
     }
 }
