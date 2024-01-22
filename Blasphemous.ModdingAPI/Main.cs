11364bd0b3a55fb093cea510e3c021de900413ad
unknown <brandenek03912@hotmail.com>
2024-01-22 09:44:56 -0500

    core: setup new project structure

---
+++ Blasphemous.ModdingAPI/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.ModdingAPI
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Main : BaseUnityPlugin
+    {
+        public static ModdingAPI ModdingAPI { get; private set; }
+
+        private void Start()
+        {
+            ModdingAPI = new ModdingAPI();
+        }
+    }
+}
---
+++ Blasphemous.ModdingAPI/ModdingAPI.cs
@@ -0,0 +1,14 @@
+﻿using ModdingAPI;
+
+namespace Blasphemous.ModdingAPI
+{
+    public class ModdingAPI : Mod
+    {
+        public ModdingAPI() : base(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION) { }
+
+        protected override void Initialize()
+        {
+            Log($"{PluginInfo.PLUGIN_NAME} has been initialized");
+        }
+    }
+}
