4358ef85f274c4f100cf3a00c51fe4c5c50a38ae
unknown <brandenek03912@hotmail.com>
2024-01-23 12:01:51 -0500

    penitence: fix serialization bug

--- Blasphemous.ModdingAPI/Penitence/ModPenitenceSystem.cs
+++ Blasphemous.ModdingAPI/Penitence/ModPenitenceSystem.cs
@@ -2,9 +2,10 @@
 
 namespace Blasphemous.ModdingAPI.Penitence;
 
-internal class ModPenitenceSystem(string id) : IPenitence
+internal class ModPenitenceSystem : IPenitence
 {
-    public string Id { get; } = id;
+    public string id;
+    public string Id => id;
 
     public bool Completed { get; set; }
     public bool Abandoned { get; set; }
@@ -17,5 +18,10 @@ internal class ModPenitenceSystem(string id) : IPenitence
     public void Deactivate()
     {
         Main.ModdingAPI.PenitenceHandler.DeactivatePenitence(Id);
+    }
+
+    public ModPenitenceSystem(string id)
+    {
+        this.id = id;
     }
 }
