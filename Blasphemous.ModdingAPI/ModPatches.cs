bbead7ea5b505557ce244572c21aa46a718a3b87
unknown <brandenek03912@hotmail.com>
2024-02-20 15:59:51 -0500

    ui: finally figure out hiding list iin submenus

--- Blasphemous.ModdingAPI/ModPatches.cs
+++ Blasphemous.ModdingAPI/ModPatches.cs
@@ -1,6 +1,12 @@
 ﻿using Framework.Managers;
+using Gameplay.UI;
+using Gameplay.UI.Others;
 using Gameplay.UI.Others.MenuLogic;
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Blasphemous.ModdingAPI;
@@ -42,11 +48,84 @@ internal class VersionNumber_Patch
         Text version = __instance.GetComponent<Text>();
         Main.ModdingAPI.BlasFont = version.font;
         Main.ModdingAPI.VersionNumber = __instance;
+        Main.ModdingAPI.MenuUI = __instance.transform.parent.parent.gameObject;
+
+        Main.ModdingAPI.LogError(__instance.transform.parent.parent.parent.DisplayHierarchy(10, true));
     }
 }
 
-//[HarmonyPatch(typeof(NewMainMenu), "Update")]
-//class Menu_Update_Patch
+[HarmonyPatch(typeof(NewMainMenu), "SetState")]
+class Menu_Update_Patch
+{
+    public static void Postfix(Animator ___animator)
+    {
+        Main.ModdingAPI.LogError("State: " + ___animator.GetInteger("STATUS"));
+    }
+}
+
+[HarmonyPatch(typeof(NewMainMenu), nameof(NewMainMenu.OptionBossRush))]
+class t1
+{
+    public static void Postfix() => Main.ModdingAPI.ShowMenu = false;
+}
+
+[HarmonyPatch(typeof(NewMainMenu), nameof(NewMainMenu.OptionCampain))]
+class t2
+{
+    public static void Postfix() => Main.ModdingAPI.ShowMenu = false;
+}
+
+[HarmonyPatch(typeof(NewMainMenu), nameof(NewMainMenu.OptionExtras))]
+class t3
+{
+    public static void Postfix() => Main.ModdingAPI.ShowMenu = false;
+}
+
+[HarmonyPatch(typeof(NewMainMenu), nameof(NewMainMenu.OptionOptions))]
+class t4
+{
+    public static void Postfix() => Main.ModdingAPI.ShowMenu = false;
+}
+
+[HarmonyPatch(typeof(NewMainMenu), nameof(NewMainMenu.ShowMenu))]//"ShowMainMenuOptions")]
+class t5
+{
+    public static void Postfix(KeepFocus ___mainMenuKeepFocus)
+    {
+        Main.ModdingAPI.Focuser = ___mainMenuKeepFocus;
+    }
+}
+
+[HarmonyPatch(typeof(NewMainMenu), nameof(NewMainMenu.ShowChooseBackground))]
+class t6
+{
+    public static void Postfix() => Main.ModdingAPI.ShowMenu = false;
+}
+
+[HarmonyPatch(typeof(NewMainMenu), "Update")]
+class t7
+{
+    public static void Postfix(bool ___menuButtonsNavEnabled, List<GameObject> ___mainMenuOptions, List<Button> ___AllButtons)
+    {
+        //Main.ModdingAPI.Nav = ___menuButtonsNavEnabled;
+
+        //Main.ModdingAPI.Log(___mainMenuOptions.Where(x => x.activeInHierarchy).Count());
+
+        foreach (var button in ___AllButtons)
+        {
+            Text text = button.transform.GetChild(1).GetComponent<Text>();
+            Main.ModdingAPI.LogError(text.name + " " + text.color);
+        }
+
+        Main.ModdingAPI.Nav = (___AllButtons.Any(x => x.transform.GetChild(1).GetComponent<Text>().color != Color.white));
+    }
+}
+
+//[HarmonyPatch(typeof(KeepFocus), "Update")]
+//class Focus_Update_Patch
 //{
-//    public static void Postfix(NewMainMenu.MenuState menuState)
+//    public static void Prefix(List<GameObject> ___allowedObjects)
+//    {
+//        Main.ModdingAPI.Nav = ___allowedObjects.Any(x => EventSystem.current.currentSelectedGameObject == x);
+//    }
 //}
--- Blasphemous.ModdingAPI/ModdingAPI.cs
+++ Blasphemous.ModdingAPI/ModdingAPI.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using Gameplay.UI.Others;
+using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,20 +14,62 @@ internal class ModdingAPI : BlasMod
 
     public Font BlasFont { get; set; }
     public VersionNumber VersionNumber { get; set; }
+    public KeepFocus Focuser { get; set; }
+    public GameObject MenuUI { get; set; }
+    public bool Nav { get; set; }
 
     protected internal override void OnLevelLoaded(string oldLevel, string newLevel)
     {
         if (newLevel != "MainMenu" || _modList != null)
             return;
 
+        CreateModList();
+    }
+
+    public bool ShowMenu
+    {
+        set
+        {
+            //if (value)
+            //    ShowModList();
+            //else
+            //    HideModList();
+        }
+    }
+
+    protected internal override void OnUpdate()
+    {
+        //LogWarning(MenuUI.activeInHierarchy);
+        //LogError(Nav);
+        //foreach (Transform child in MenuUI.transform)
+        //{
+        //    LogWarning(child.name + ": " + child.gameObject.activeInHierarchy);
+        //}
+
+        _modList?.SetActive(LoadStatus.CurrentScene == "MainMenu" && Nav);
+        //LogWarning(ShowMenu);
+        //LogWarning("VN active: " + (VersionNumber != null && VersionNumber.isActiveAndEnabled));
+    }
+
+    public void ShowModList()
+    {
+        if (_modList == null)
+            CreateModList();
+
+        _modList?.SetActive(true);
+    }
+
+    public void HideModList()
+    {
+        _modList?.SetActive(false);
+    }
+
+    private void CreateModList()
+    {
         // Find highres canvas
         Canvas canvas = Object.FindObjectsOfType<Canvas>().FirstOrDefault(x => x.name == "Game UI (No Pixel Perfect)");
         if (canvas == null)
             return;
-        foreach (Transform child in canvas.transform)
-        {
-            LogError(child.name);
-        }
 
         // Create text for mod list
         StringBuilder sb = new StringBuilder($"Loaded {Main.ModLoader.AllMods.Count()} mods:").AppendLine();
@@ -53,10 +96,5 @@ internal class ModdingAPI : BlasMod
         t.fontSize = 32;
 
         _modList = t.gameObject;
-    }
-
-    protected internal override void OnUpdate()
-    {
-        LogWarning("VN active: " + (VersionNumber != null && VersionNumber.isActiveAndEnabled));
     }
 }
