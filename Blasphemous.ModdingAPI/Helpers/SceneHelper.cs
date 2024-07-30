6593140ee6980d1acf34efe285a26b7a13b59058
unknown <brandenek03912@hotmail.com>
2024-07-30 11:09:53 -0400

    scene: replace loadstatus with scenehelper

---
+++ Blasphemous.ModdingAPI/Helpers/SceneHelper.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Blasphemous.ModdingAPI.Helpers;
+
+/// <summary>
+/// Provides information about the currently loaded scene
+/// </summary>
+public static class SceneHelper
+{
+    /// <summary>
+    /// Whether an actual in-game scene is loaded
+    /// </summary>
+    public static bool GameSceneLoaded => AnySceneLoaded && !MenuSceneLoaded;
+
+    /// <summary>
+    /// Whether the main menu scene is loaded
+    /// </summary>
+    public static bool MenuSceneLoaded => Main.ModLoader.CurrentScene == "MainMenu";
+
+    /// <summary>
+    /// Whether the main menu or an in-game scene is loaded
+    /// </summary>
+    public static bool AnySceneLoaded => Main.ModLoader.CurrentScene.Length > 0;
+
+    /// <summary>
+    /// The name of the currently loaded scene, or ""
+    /// </summary>
+    public static string CurrentScene => Main.ModLoader.CurrentScene;
+}
