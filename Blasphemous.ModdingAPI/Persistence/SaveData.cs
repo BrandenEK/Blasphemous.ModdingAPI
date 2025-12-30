ab6895a701912abb328e58dc0319b1781958d4c8
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-12-30 13:29:03 -0500

    core: mark old persistent system as obsolete (#28)

--- Blasphemous.ModdingAPI/Persistence/SaveData.cs
+++ Blasphemous.ModdingAPI/Persistence/SaveData.cs
@@ -5,4 +5,5 @@ namespace Blasphemous.ModdingAPI.Persistence;
 /// <summary>
 /// Used to save and load persistent data for a mod
 /// </summary>
+[System.Obsolete("Use the new ISlotPersistentMod instead")]
 public abstract class SaveData(string persistentId) : PersistentManager.PersistentData(persistentId) { }
