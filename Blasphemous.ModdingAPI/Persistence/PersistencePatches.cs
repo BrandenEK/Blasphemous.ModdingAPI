9eb6c8714fff376ff3c16409019fb83d65a5f01d
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-12-30 13:20:09 -0500

    persistence: hook up slot saving

---
+++ Blasphemous.ModdingAPI/Persistence/PersistencePatches.cs
@@ -0,0 +1,28 @@
+﻿using Framework.Managers;
+using HarmonyLib;
+
+namespace Blasphemous.ModdingAPI.Persistence;
+
+[HarmonyPatch(typeof(PersistentManager), nameof(PersistentManager.ResetPersistence))]
+class PersistentManager_ResetPersistence_Patch
+{
+    public static void Postfix() => SlotSaveData.Reset();
+}
+
+[HarmonyPatch(typeof(PersistentManager), nameof(PersistentManager.SaveGame), typeof(int), typeof(bool))]
+class PersistentManager_SaveGame_Patch
+{
+    public static void Postfix(int slot) => SlotSaveData.Save(slot);
+}
+
+[HarmonyPatch(typeof(PersistentManager), nameof(PersistentManager.LoadGame))]
+class PersistentManager_LoadGame_Patch
+{
+    public static void Postfix(int slot) => SlotSaveData.Load(slot);
+}
+
+[HarmonyPatch(typeof(PersistentManager), nameof(PersistentManager.DeleteSaveGame))]
+class PersistentManager_DeleteSaveGame_Patch
+{
+    public static void Postfix(int slot) => SlotSaveData.Delete(slot);
+}
